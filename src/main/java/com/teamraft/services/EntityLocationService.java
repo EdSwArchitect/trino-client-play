47d2c07bd2595915bfd2b89d1cf14adf0f48cd74
Ed Brown <ebrown@teamraft.com>
2023-11-09 11:21:46 -0500

    Foo

--- src/main/java/com/teamraft/services/EntityLocationService.java
+++ src/main/java/com/teamraft/services/EntityLocationService.java
@@ -1,6 +1,8 @@
 package com.teamraft.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.teamraft.model.Entity;
+import com.teamraft.model.EntityLocation;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,11 +15,11 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Properties;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class EntityLocationService {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     static final String DB_URL = "jdbc:trino://my-trino:8080/entityLocation/comms-broker?user=admin";
     private XmlHelper xmlHelper = new XmlHelper();
     private KafkaProducer<String, String> producer;
@@ -41,15 +43,15 @@ public class EntityLocationService {
     }
 
     public void sendIt(List<String> entities) {
-        System.out.printf("The list of records is %d in size%n", entities.size());
+        System.out.printf("The list of records is %d in size%n for entityLocation", entities.size());
 
         entities.forEach(entity -> {
 
             try {
-                Entity e = new Entity(entity, xmlHelper);
+                EntityLocation l = new EntityLocation(new Entity(entity, xmlHelper));
 
                 ProducerRecord<String, String> record =
-                        new ProducerRecord<>(topicName, UUID.randomUUID().toString(), e.toJson());
+                        new ProducerRecord<>(topicName, l.getId(), l.toJson());
                 Future<RecordMetadata> future = producer.send(record);
 
                 future.get().hasOffset();
