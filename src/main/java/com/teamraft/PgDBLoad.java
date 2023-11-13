b135f7d2194a68253c77c8812b1ec31c73514e0f
Ed Brown <ebrown@teamraft.com>
2023-11-13 18:24:38 -0500

    postgresql

--- src/main/java/com/teamraft/PgDBLoad.java
+++ src/main/java/com/teamraft/PgDBLoad.java
@@ -6,11 +6,13 @@ import java.io.File;
 import java.util.List;
 
 public class PgDBLoad {
-    public static void main(String... args) {
+    public static void main(String... args) throws ClassNotFoundException {
         // String bootstrapServers, String topicName
         File dir = new File(args[0]);
 
         final EntityService entityService = new EntityService();
+
+        Class.forName("");
 
         for (File file : dir.listFiles()) {
             System.out.printf("Processing file %s%n", file.getAbsolutePath());
